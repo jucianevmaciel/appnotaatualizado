f4762fc2f05663d431b88bdf62cebf191f174c45
jucianevmaciel <jucianevalemaciel123@gmail.com>
2023-09-09 13:29:57 -0300

    apicativo de notas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,12 +10,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.example.myapplication.R;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText textnome, nota1, nota2, nota3;
     private Context context;
     private SharedPreferences sharedPrefs;
+    private List<String> pessoas; // Lista para controlar as chaves das pessoas
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         context = getApplicationContext();
         setContentView(R.layout.activity_main);
         sharedPrefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+        pessoas = new ArrayList<>(); // Inicializa a lista
+
     }
 
     public void salvar(View view) {
@@ -45,35 +51,37 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(context, "Digite todas as notas", Toast.LENGTH_SHORT).show();
             return;
         }
-
+        String chavePessoa = "pessoa_" + nome;
         SharedPreferences.Editor editor = sharedPrefs.edit();
         editor.putString(nome + "_nota1", note1);
         editor.putString(nome + "_nota2", note2);
         editor.putString(nome + "_nota3", note3);
         editor.apply();
-
+        pessoas.add(chavePessoa);
 
         // Se desejar, você pode adicionar uma ação após salvar os dados.
         // Exemplo: exibir uma mensagem de sucesso.
         Toast.makeText(context, "Dados salvos com sucesso", Toast.LENGTH_SHORT).show();
     }
     public void carregar(View view) {
-        sharedPrefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+        // Itera pelas chaves das pessoas na lista e carrega os dados
+        for (String chavePessoa : pessoas) {
+            String nome = chavePessoa.substring(7); // Obtém o nome da pessoa
+            String nota1 = sharedPrefs.getString(chavePessoa + "_nota1", "");
+            String nota2 = sharedPrefs.getString(chavePessoa + "_nota2", "");
+            String nota3 = sharedPrefs.getString(chavePessoa + "_nota3", "");
 
-        String nome = textnome.getText().toString();
-        String nota1 = sharedPrefs.getString(nome + "_nota1", "");
-        String nota2 = sharedPrefs.getString(nome + "_nota2", "");
-        String nota3 = sharedPrefs.getString(nome + "_nota3", "");
+            // Faça algo com os dados de cada pessoa (por exemplo, exiba-os)
+            // Você pode iniciar uma nova Activity aqui para exibir os dados
+            Intent telaResultado = new Intent(getApplicationContext(), Resultado.class);
+            telaResultado.putExtra("Nome", nome);
+            telaResultado.putExtra("Nota 1", nota1);
+            telaResultado.putExtra("Nota 2", nota2);
+            telaResultado.putExtra("Nota 3", nota3);
+            startActivity(telaResultado);
+        }
 
-        // Crie um Intent para abrir a tela "Resultado" e passe os dados como extras
-        Intent telaResultado = new Intent(getApplicationContext(), Resultado.class);
-        telaResultado.putExtra("Nome", nome);
-        telaResultado.putExtra("Nota 1", nota1);
-        telaResultado.putExtra("Nota 2", nota2);
-        telaResultado.putExtra("Nota 3", nota3);
-        startActivity(telaResultado);
-
+        // Exibe uma mensagem de sucesso
         Toast.makeText(this, "Dados carregados com sucesso", Toast.LENGTH_SHORT).show();
     }
-
 }
--- app/src/main/java/com/example/myapplication/Resultado.java
+++ app/src/main/java/com/example/myapplication/Resultado.java
@@ -9,34 +9,48 @@ import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 public class Resultado extends AppCompatActivity {
 
-    private TextView textNome, textnota1, textnota2, textnota3;
-    private Context context;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resultado);
 
-                // Recupere os extras passados da atividade anterior
-                Intent intent = getIntent();
-                String nome = intent.getStringExtra("Nome");
-                String nota1 = intent.getStringExtra("Nota 1");
-                String nota2 = intent.getStringExtra("Nota 2");
-                String nota3 = intent.getStringExtra("Nota 3");
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_resultado);
 
-                // Exiba os valores em TextViews ou onde desejar na tela de "Resultado"
-                 textNome = findViewById(R.id.textNome);
-                 textnota1 = findViewById(R.id.textnota1);
-                 textnota2 = findViewById(R.id.textnota2);
-                 textnota3 = findViewById(R.id.textnota3);
+            ListView listView = findViewById(R.id.listView);
 
-                textNome.setText("Nome: " + nome);
-                textnota1.setText("Nota 1: " + nota1);
-                textnota2.setText("Nota 2: " + nota2);
-                textnota3.setText("Nota 3: " + nota3);
+            // Obtenha os dados das intenções extras
+            ArrayList<String> nomes = getIntent().getStringArrayListExtra("Nomes");
+            ArrayList<String> notas1 = getIntent().getStringArrayListExtra("Notas1");
+            ArrayList<String> notas2 = getIntent().getStringArrayListExtra("Notas2");
+            ArrayList<String> notas3 = getIntent().getStringArrayListExtra("Notas3");
+
+            // Crie uma lista de strings para exibir os dados
+            ArrayList<String> dados = new ArrayList<>();
+
+            // Preencha a lista com os dados das pessoas
+            for (int i = 0; i < nomes.size(); i++) {
+                String nome = nomes.get(i);
+                String nota1 = notas1.get(i);
+                String nota2 = notas2.get(i);
+                String nota3 = notas3.get(i);
+
+                // Adicione os dados ao formato desejado na lista
+                dados.add("Nome: " + nome + "\nNota 1: " + nota1 + "\nNota 2: " + nota2 + "\nNota 3: " + nota3);
             }
+
+            // Crie um adaptador para preencher o ListView com os dados
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dados);
+
+            // Configure o ListView para usar o adaptador
+            listView.setAdapter(adapter);
+        }
 
     public  void voltar (View view){
         finish();
